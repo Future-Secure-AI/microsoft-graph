6b5d05f2f0186371255d642c53d5ffc5401aaced
Ben Thompson <ben.thompson@futuresecure.ai>
2025-02-26 09:49:30 +1000

    Tidied configuration binding error handling

--- src/repositories/Configuration.ts
+++ src/repositories/Configuration.ts
@@ -6,30 +6,34 @@ export default class Configuration {
     public readonly azureClientId: string;
     public readonly azureClientSecret: string;
     public readonly azureScopes: string;
+    private readonly failedBinding: string[] = [];
 
     public constructor() {
-        this.azureTenantId = Configuration.requireString("AZURE_TENANT_ID");
-        this.azureClientId = Configuration.requireString("AZURE_CLIENT_ID");
-        this.azureClientSecret = Configuration.requireString("AZURE_CLIENT_SECRET");
-        this.azureScopes = Configuration.optionalString("AZURE_SCOPES") ?? "https://graph.microsoft.com/.default"
+        this.azureTenantId = this.tryBindString("AZURE_TENANT_ID");
+        this.azureClientId = this.tryBindString("AZURE_CLIENT_ID");
+        this.azureClientSecret = this.tryBindString("AZURE_CLIENT_SECRET");
+        this.azureScopes = this.tryBindString("AZURE_SCOPES", "https://graph.microsoft.com/.default")
+
+        this.faultOnFailedBindings();
         Object.freeze(this);
     }
 
-    private static requireString(env: string): string {
-        const value = Configuration.optionalString(env)
-        if (value === null) {
-            // eslint-disable-next-line no-console
-            console.error(`\x1b[31mEnvironment variable '${env}' missing or empty.\x1b[0m`);
-            process.exit(1);
+    private tryBindString(env: string, defaultValue: string | null = null): string {
+        const value = process.env[env]?.trim() ?? "";
+
+        if (value === "") {
+            if (defaultValue === null) this.failedBinding.push(env);
+            return defaultValue ?? "";
         }
+
         return value;
     }
 
-    private static optionalString(env: string): string | null {
-        const value = process.env[env];
-        // eslint-disable-next-line no-undefined
-        if (value === undefined) return null;
-        if (value.trim() === "") return null;
-        return value;
+    private faultOnFailedBindings() {
+        if (this.failedBinding.length == 0) return;
+
+        // eslint-disable-next-line no-console
+        console.error(`\x1b[31mEnvironment variable(s) ${this.failedBinding.join(", ")} missing or empty.\x1b[0m`);
+        process.exit(1);
     }
 }
