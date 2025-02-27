8fb42bec0364a083573ea7bee8e29de99afd73f7
Ben Thompson <ben.thompson@futuresecure.ai>
2025-02-27 10:02:13 +1000

    Folder refactor

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import type { DriveId, DriveReference, ItemId, ItemReference } from "./services/graphApi/drive.js";
-import type { SiteId, SiteReference } from "./services/graphApi/site.js";
-import { getUsedRangeValues, type WorksheetReference, type WorksheetName } from "./services/graphApi/workbook.js";
+import type { DriveId, DriveReference, ItemId, ItemReference } from "./graphApi/drive.js";
+import type { SiteId, SiteReference } from "./graphApi/site.js";
+import { getUsedRangeValues, type WorksheetReference, type WorksheetName } from "./graphApi/workbook.js";
 
 const siteId = "<INSERT-SITE-ID>" as SiteId;
 const driveId = "<INSERT-DRIVE-ID>" as DriveId;
