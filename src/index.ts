5b09921a699de1affb2e3d360544577a28b935f7
Ben Thompson <ben.thompson@futuresecure.ai>
2025-03-13 08:30:30 +1000

    Considered dual CLI/native invocation approach.

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,73 @@
+import { fileURLToPath } from "url";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
-import type { DriveId, DriveRef, ItemId, ItemRef } from "./microsoftGraph/drive.js";
-import type { SiteId, SiteRef } from "./microsoftGraph/site.js";
-import { getUsedRangeValues, type WorksheetName, type WorksheetRef } from "./microsoftGraph/workbook.js";
+import type { DriveId, ItemId } from "./microsoftGraph/drive.js";
+import type { SiteId } from "./microsoftGraph/site.js";
+import { getUsedRangeValues, type WorksheetName } from "./microsoftGraph/workbook.js";
 
-const argv = await yargs(hideBin(process.argv))
-	.options({
-		siteId: { type: "string", demandOption: true, coerce: (v) => v as SiteId },
-		driveId: { type: "string", demandOption: true, coerce: (v) => v as DriveId },
-		itemId: { type: "string", demandOption: true, coerce: (v) => v as ItemId },
-		worksheetName: { type: "string", demandOption: true, coerce: (v) => v as WorksheetName },
-	})
-	.parse();
+export type Arguments = {
+	siteId: SiteId;
+	driveId: DriveId;
+	itemId: ItemId;
+	worksheetName: WorksheetName;
+};
 
-const siteReference: SiteRef = {
-	site: argv.siteId,
-};
-const driveReference: DriveRef = {
-	...siteReference,
-	drive: argv.driveId,
-};
-const itemReference: ItemRef = {
-	...driveReference,
-	item: argv.itemId,
-};
-const worksheetReference: WorksheetRef = {
-	...itemReference,
-	worksheet: argv.worksheetName,
-};
-const cells = await getUsedRangeValues(worksheetReference);
+/*
+ * Core logic goes here. But don't call this function directly - use `runNative` or `runCli` instead.
+ */
+const run = async (args: Arguments) => {
+	const cells = await getUsedRangeValues({
+		site: args.siteId,
+		drive: args.driveId,
+		item: args.itemId,
+		worksheet: args.worksheetName,
+	});
 
-console.log(cells.values);
+	console.log(cells.values);
+};
+
+/*
+ * Called by Flowise during normal use.
+ */
+export const runNative = async (args: Arguments): Promise<string[]> => {
+	const info = console.info;
+	const warn = console.warn;
+	const error = console.error;
+
+	const messages: string[] = [];
+
+	console.info = (message: string): void => {
+		messages.push(`[INFO] ${message}`);
+		info(message);
+	};
+	console.warn = (message: string): void => {
+		messages.push(`[WARN] ${message}`);
+		warn(message);
+	};
+	console.error = (message: string): void => {
+		messages.push(`[ERROR] ${message}`);
+		error(message);
+	};
+
+	await run(args);
+	return messages;
+};
+
+/*
+ * Called by CLI during development.
+ */
+const runCli = async () => {
+	const args = await yargs(hideBin(process.argv))
+		.options({
+			siteId: { type: "string", demandOption: true, coerce: (v) => v as SiteId },
+			driveId: { type: "string", demandOption: true, coerce: (v) => v as DriveId },
+			itemId: { type: "string", demandOption: true, coerce: (v) => v as ItemId },
+			worksheetName: { type: "string", demandOption: true, coerce: (v) => v as WorksheetName },
+		})
+		.parse();
+
+	await run(args);
+};
+
+const isCliInvoked = process.argv[1] === fileURLToPath(import.meta.url);
+if (isCliInvoked) await runCli();
