732a77049fb88ef6409db168284b6d5ed5068de7
Ben Thompson <ben.thompson@futuresecure.ai>
2025-03-17 11:11:18 +1000

    Tidy

--- src/microsoftGraph/utils/stringCaseConversion.ts
+++ src/microsoftGraph/utils/stringCaseConversion.ts
@@ -1,3 +1,3 @@
-export default function kebabToCamelCase(str: string): string {
+export function kebabToCamelCase(str: string): string {
     return str.replace(/-([a-z])/g, (_: string, letter: string) => letter.toUpperCase());
 }
