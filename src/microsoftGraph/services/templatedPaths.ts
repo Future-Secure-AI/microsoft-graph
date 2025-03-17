e5f2d45324b49a31bcd9dcee2312aa1724bc3041
Ben Thompson <ben.thompson@futuresecure.ai>
2025-03-18 08:05:04 +1000

    Fixed token matching

--- src/microsoftGraph/services/templatedPaths.ts
+++ src/microsoftGraph/services/templatedPaths.ts
@@ -2,6 +2,8 @@ import BadTemplateError from "../errors/BadTemplateError.js";
 import type { GraphPath } from "../models/GraphOperation.js";
 import { kebabToCamelCase } from "./stringCaseConversion.js";
 
+const argmentPattern = /\{([a-z-]+)\}/g;
+
 /** Create a GraphAPI path based on a given template and arguments. Escaping is automatically handeld */
 export function generatePath(template: string, args: Record<string, string>): GraphPath {
     if (!template.startsWith("/")) {
@@ -11,8 +13,8 @@ export function generatePath(template: string, args: Record<string, string>): Gr
         throw new BadTemplateError("Path template must not contain newlines.");
     }
 
-    return template.replace(/{(\w+)}/g, (key: string): string => {
-        const camelCaseKey = kebabToCamelCase(key);
+    return template.replace(argmentPattern, (_: string, match: string): string => {
+        const camelCaseKey = kebabToCamelCase(match);
         const value = args[camelCaseKey as keyof typeof args];
         if (value === undefined) {
             throw new BadTemplateError(`Path template references argument '${camelCaseKey}' however no such argument provided.`);
