1e03bd931b43fda31570ba389c95c501a499cf8c
Ben Thompson <ben.thompson@futuresecure.ai>
2025-03-18 13:47:00 +1000

    Refactor API core

--- src/microsoftGraph/graphApi.ts
+++ src/microsoftGraph/graphApi.ts
@@ -3,13 +3,14 @@ import RequestFailedError from "./errors/RequestFailedError.js";
 import type { GraphOperation } from "./models/GraphOperation.js";
 import type { Scope } from "./models/Scope.js";
 import { getCurrentAccessToken } from "./services/accessToken.js";
+import { isHttpOk } from "./services/httpStatus.js";
 
 const authenticationScope = "https://graph.microsoft.com/.default" as Scope;
-const batchEndpoint = "https://graph.microsoft.com/v1.0/$batch";
+const endpoint = "https://graph.microsoft.com/v1.0/$batch";
 const minBatchCalls = 1;
 const maxBatchCalls = 20; // https://learn.microsoft.com/en-us/graph/json-batching?tabs=http#batch-size-limitations
 
-type Response = {
+type ReplyPayload = {
     responses: {
         id: string;
         status: number;
@@ -24,11 +25,31 @@ type ExecutionResults<T> = {
 
 /** Execute GraphAPI batch with up to 20 requests to be executed as a batch. */
 export async function execute<T extends GraphOperation<unknown>[]>(...ops: T): Promise<ExecutionResults<T>> {
-    const accessToken = await getCurrentAccessToken(authenticationScope);
-
     if (ops.length < minBatchCalls || ops.length > maxBatchCalls) {
         throw new InvalidArgumentError(`Requires at least ${minBatchCalls} and at most ${maxBatchCalls} calls`);
     }
+
+    const requestPayload = await composeRequestPayload<T>(ops);
+
+    const response = await fetch(endpoint, requestPayload);
+
+    if (!isHttpOk(response.status)) {
+        const responsePayload = await response.json();
+
+        throw new RequestFailedError(
+            `GraphAPI batch failed with HTTP ${response.status}\n` +
+            `Operations: ${JSON.stringify(ops, null, 2)}\n` +
+            `Response: ${JSON.stringify(responsePayload, null, 2)}`
+        );
+    }
+
+    const replyPayload = await response.json() as ReplyPayload;
+
+    return parseResponses<T>(replyPayload, ops);
+}
+
+async function composeRequestPayload<T extends GraphOperation<unknown>[]>(ops: T) {
+    const accessToken = await getCurrentAccessToken(authenticationScope);
 
     const requestBody = {
         requests: ops.map((call, index) => ({
@@ -36,12 +57,12 @@ export async function execute<T extends GraphOperation<unknown>[]>(...ops: T): P
             method: call.method,
             url: call.path,
             headers: call.headers,
-            body: call.body == null ? undefined : call.body,
+            body: call.body === null ? undefined : call.body,
             dependsOn: call.dependsOn?.map((id) => id.toString())
         }))
     };
 
-    const response = await fetch(batchEndpoint, {
+    const requestPayload = {
         method: "POST",
         headers: {
             'authorization': `Bearer ${accessToken}`,
@@ -49,27 +70,19 @@ export async function execute<T extends GraphOperation<unknown>[]>(...ops: T): P
             'content-type': 'application/json'
         },
         body: JSON.stringify(requestBody)
-    });
+    };
 
-    if (!response.ok) {
-        const responseBody = await response.json();
+    return requestPayload;
+}
 
-        throw new RequestFailedError(
-            `GraphAPI batch failed with HTTP ${response.status}\n` +
-            `Batch: ${JSON.stringify(requestBody, null, 2)}\n` +
-            `Body: ${JSON.stringify(responseBody, null, 2)}`
-        );
-    }
-
-    const json = await response.json() as Response;
-
-    if (!json.responses) {
+function parseResponses<T extends GraphOperation<unknown>[]>(replyPayload: ReplyPayload, ops: T) {
+    if (!replyPayload.responses) {
         throw new RequestFailedError("Batch request did not return any responses");
     }
 
     const results: unknown[] = []; // A little ugly, but effective without blatantly compromising type safety. Is there a neater way?
 
-    for (const response of json.responses) {
+    for (const response of replyPayload.responses) {
         const index = Number.parseInt(response.id, 10);
 
         const headers: Record<string, string> = {};
@@ -81,12 +94,12 @@ export async function execute<T extends GraphOperation<unknown>[]>(...ops: T): P
 
         let body = response.body;
 
-        if (contentType?.startsWith("application/json") && typeof response.body === "string") { // Batch API sometimes returns base64 encoded JSON
+        // Batch API sometimes returns base64 encoded JSON, correct for that
+        if (contentType?.startsWith("application/json") && typeof response.body === "string") {
             body = JSON.parse(atob(response.body));
         }
 
-        //   "Content-Type": "application/json;odata.metadata=minimal;odata.streaming=true;IEEE754Compatible=false;charset=utf-8"
-        if (response.status < 200 || response.status > 299) {
+        if (!isHttpOk(response.status)) {
             const op = JSON.stringify(ops[index], null, 2);
             const bodyRaw = JSON.stringify(body, null, 2);
 
