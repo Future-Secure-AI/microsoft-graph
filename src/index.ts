fd6b2e9e43c0d3d9ab16f81a8c580b1e329f5b09
Ben Thompson <ben.thompson@futuresecure.ai>
2025-03-18 12:05:54 +1000

    Shakedown tidy

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,27 @@
 
 import { info } from "./log.js";
 import { execute } from "./microsoftGraph/graphApi.js";
-import { openDefaultDrive } from "./microsoftGraph/helpers/openDrive.js";
-import { createWorkbookWithSession, } from "./microsoftGraph/helpers/openWorkbook.js";
+import { openDefaultDrive as getDefaultDriveRef } from "./microsoftGraph/helpers/openDrive.js";
+import { closeSessionAndDeleteWorkbook, createWorkbookAndOpenSession, workbookFileExtension, } from "./microsoftGraph/helpers/openWorkbook.js";
 import type { DriveItemPath } from "./microsoftGraph/models/DriveItemPath.js";
-import deleteDriveItem from "./microsoftGraph/operations/driveItem/deleteDriveItem.js";
 import listDriveItems from "./microsoftGraph/operations/driveItem/listDriveItems.js";
-import closeWorkbookSession from "./microsoftGraph/operations/workbookSession/closeWorkbookSession.js";
-import { defaultDriveId, defaultSiteId } from "./microsoftGraph/services/configuration.js";
-import { sleep } from "./microsoftGraph/services/sleep.js";
 
 export async function run(): Promise<void> {
 	const testPath = "/test" as DriveItemPath;
-	const testFile = `/${testPath}/${crypto.randomUUID()}.xlsx` as DriveItemPath;
+	const testFile = `/${testPath}/${crypto.randomUUID()}${workbookFileExtension}` as DriveItemPath;
+	const driveRef = getDefaultDriveRef();
 
 	info("Creating file...");
-	const driveRef = openDefaultDrive();
-	const workbookRef = await createWorkbookWithSession(driveRef, testFile);
+	const workbookRef = await createWorkbookAndOpenSession(driveRef, testFile);
 
 	info("Listing files...");
-	const [itemList] = await execute(listDriveItems({ siteId: defaultSiteId, driveId: defaultDriveId }, testPath));
+	const [itemList] = await execute(listDriveItems(driveRef, testPath));
 	for (const driveItem of itemList.value) {
 		info(` - ${driveItem.name}`);
 	}
 
 	info("Deleting file...");
-	await execute(closeWorkbookSession(workbookRef));
-	await sleep(1000); // Close session takes a moment to release the file lock
-	await execute(deleteDriveItem(workbookRef));
+	await closeSessionAndDeleteWorkbook(workbookRef);
 
 	info("Done.");
 }
--- src/microsoftGraph/helpers/openWorkbook.ts
+++ src/microsoftGraph/helpers/openWorkbook.ts
@@ -1,12 +1,16 @@
+import InvalidArgumentError from "../errors/InvalidArgumentError.js";
 import { execute } from "../graphApi.js";
 import type { DriveItemId } from "../models/DriveItemId.js";
 import type { DriveItemPath } from "../models/DriveItemPath.js";
 import type { DriveItemRef } from "../models/DriveItemRef.js";
 import type { DriveRef } from "../models/DriveRef.js";
 import type { WorkbookRef } from "../models/WorkbookRef.js";
+import deleteDriveItem from "../operations/driveItem/deleteDriveItem.js";
 import createWorkbookInner from "../operations/workbook/createWorkbook.js";
+import closeWorkbookSession from "../operations/workbookSession/closeWorkbookSession.js";
 import createWorkbookSession from "../operations/workbookSession/createWorkbookSession.js";
 import { defaultDriveId, defaultSiteId } from "../services/configuration.js";
+import { sleep } from "../services/sleep.js";
 
 /** Opinionated convenience helper to start a workbook session. Includes starting a session. */
 export async function openWorkbookWithSession(itemRef: DriveItemRef): Promise<WorkbookRef> {
@@ -33,12 +37,14 @@ export async function openWorkbookInDefaultDrive(itemId: DriveItemId): Promise<W
     }
 }
 
-export async function createWorkbookWithSession(driveRef: DriveRef, itemPath: DriveItemPath): Promise<WorkbookRef> {
+export const workbookFileExtension = ".xlsx";
+export async function createWorkbookAndOpenSession(driveRef: DriveRef, itemPath: DriveItemPath): Promise<WorkbookRef> {
+    if (!itemPath.endsWith(workbookFileExtension)) {
+        throw new InvalidArgumentError(`Item path must end with '${workbookFileExtension}'`);
+    }
+
     const [item] = await execute(createWorkbookInner(driveRef, itemPath));
 
-
-    //JSON.stringify(JSON.parse(atob(response.body)), null,2)
-    
     if (item.id === undefined) {
         throw new Error("Item id is undefined");
     }
@@ -56,4 +62,10 @@ export async function createWorkbookWithSession(driveRef: DriveRef, itemPath: Dr
     }
 
     return workbookRef;
+}
+
+export async function closeSessionAndDeleteWorkbook(workbookRef: WorkbookRef): Promise<void> {
+    await execute(closeWorkbookSession(workbookRef));
+    await sleep(1000); // Close session takes a moment to release the file lock
+    await execute(deleteDriveItem(workbookRef));
 }
