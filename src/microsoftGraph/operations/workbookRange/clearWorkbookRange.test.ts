903c09186f6ef368c2c67e9cede01decafe88f39
Ben Thompson <ben.thompson@futuresecure.ai>
2025-03-19 10:18:22 +1000

    Added clearWorkbookRange tests

---
+++ src/microsoftGraph/operations/workbookRange/clearWorkbookRange.test.ts
@@ -0,0 +1,42 @@
+import { describe, expect, it } from "vitest";
+import { executeSingle } from "../../graphApi.js";
+import type { WorkbookRangeAddress } from "../../models/WorkbookRangeAddress.js";
+import { defaultDriveRef } from "../../services/configuration.js";
+import { driveItemPath, driveItemRef, generateTempFileName } from "../../services/driveItem.js";
+import { sleep } from "../../services/sleep.js";
+import { workbookRangeRef } from "../../services/workbookRange.js";
+import deleteDriveItem from "../driveItem/deleteDriveItem.js";
+import createWorkbook from "../workbook/createWorkbook.js";
+import createWorkbookWorksheet from "../workbookWorksheet/createWorkbookWorksheet.js";
+import getWorkbookRange from "./getWorkbookRange.js";
+import updateWorkbookRange from "./updateWorkbookRange.js";
+import clearWorkbookRange from "./clearWorkbookRange.js";
+
+describe("clearWorkbookRange", () => {
+    it("can clear a range in an existing workbook", async () => {
+        const address = "A1:B2" as WorkbookRangeAddress;
+        const values = [[1, 2], [3, 4]];
+
+        const workbookName = generateTempFileName("xlsx");
+        const workbookPath = driveItemPath(workbookName);
+        const workbook = await executeSingle(createWorkbook(defaultDriveRef, workbookPath));
+        const workbookRef = driveItemRef(defaultDriveRef, workbook.id);
+        const worksheet = await executeSingle(createWorkbookWorksheet(workbookRef));
+
+        try {
+            const rangeRef = workbookRangeRef(workbookRef, worksheet.id, address);
+
+            await executeSingle(updateWorkbookRange(rangeRef, {
+                values: values
+            }));
+
+            await executeSingle(clearWorkbookRange(rangeRef));
+
+            const clearedRange = await executeSingle(getWorkbookRange(rangeRef));
+            expect(clearedRange.values).toEqual([["", ""], ["", ""]]);
+        } finally {
+            await sleep(1000);
+            await executeSingle(deleteDriveItem(workbookRef));
+        }
+    }, { timeout: 10000 });
+});
