c2c65f92222ea989cd6bd2f55ad3af8701173f0c
Ben Thompson <ben.thompson@futuresecure.ai>
2025-03-21 10:09:13 +1000

    Fixed reference

--- src/microsoftGraph/tasks/createWorkbookAndStartSession.ts
+++ src/microsoftGraph/tasks/createWorkbookAndStartSession.ts
@@ -2,11 +2,11 @@ import type { DriveItemPath } from "../models/DriveItemPath.ts";
 import type { DriveRef } from "../models/DriveRef.ts";
 import type { WorkbookRef } from "../models/WorkbookRef.ts";
 import createWorkbook from "../operations/workbook/createWorkbook.ts";
+import createWorkbookSession from "../operations/workbookSession/createWorkbookSession.ts";
 import { driveItemRef } from "../services/driveItem.ts";
-import startWorkbookSession from "./startWorkbookSession.ts";
 
 export default async function createWorkbookAndStartSession(driveRef: DriveRef, itemPath: DriveItemPath): Promise<WorkbookRef> {
     const item = await createWorkbook(driveRef, itemPath);
     const itemRef = driveItemRef(driveRef, item.id);
-    return await startWorkbookSession(itemRef);
+    return await createWorkbookSession(itemRef);
 }
