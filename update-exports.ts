57267a5b43e716da56e8739cf8e7ae1046716427
Ben Thompson <ben.thompson@futuresecure.ai>
2025-03-26 14:19:05 +1000

    Theoretical fix of generated indentation

--- update-exports.ts
+++ update-exports.ts
@@ -54,5 +54,5 @@ walk(distDir);
 
 pkg.exports = exportsMap;
 
-writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + "\n");
+writeFileSync(pkgPath, JSON.stringify(pkg, null, 1) + "\n");
 console.log("✅ package.json exports updated with", Object.keys(exportsMap).length, "entries");
