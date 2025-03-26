e1486d16658cc78505654efb5c7366bcecc48ead
Ben Thompson <ben.thompson@futuresecure.ai>
2025-03-26 14:18:47 +1000

    Added userSendMail

---
+++ src/operations/user/userSendMail.test.ts
@@ -0,0 +1,18 @@
+
+
+// describe("userSendMail", () => {
+//     it("can queue email for delivery", async () => {
+//         await userSendMail("ai@msftfuturesecureai.onmicrosoft.com", {
+//             subject: "Hello from a service app",
+//             body: {
+//                 contentType: "text",
+//                 content: "This email was sent by a daemon app."
+//             },
+//             toRecipients: [
+//                 {
+//                     emailAddress: { address: "ben.thompson@futuresecure.ai" }
+//                 }
+//             ]
+//         });
+//     });
+// });
---
+++ src/operations/user/userSendMail.ts
@@ -0,0 +1,29 @@
+import type { Message } from "@microsoft/microsoft-graph-types";
+import InvalidArgumentError from "../../errors/InvalidArgumentError.ts";
+import { operation } from "../../graphApi.ts";
+import type { GraphOperation, GraphPath } from "../../models/GraphOperation.ts";
+
+// Overview of SendMail process: https://learn.microsoft.com/en-us/graph/outlook-things-to-know-about-send-mail
+
+const emailPattern = /^[a-zA-Z0-9._\-@]+$/ // Basic, but intended just to avoid URL escaping issues
+
+/** Send an email from the current or a specified user. @see https://learn.microsoft.com/en-us/graph/api/user-sendmail */
+export default function userSendMail(sender: string | null, message: Message): GraphOperation<void> {
+    if (sender && !emailPattern.test(sender)) {
+        throw new InvalidArgumentError(`Invalid email address. Must match pattern '${emailPattern}'`);
+    }
+
+    const path = sender ? `/users/${sender}/sendMail` : "/me/sendMail";
+
+    return operation({
+        method: "POST",
+        path: path as GraphPath,
+        headers: {
+            "content-type": "application/json",
+        },
+        body: {
+            message
+        },
+        responseTransform: () => undefined
+    });
+};
