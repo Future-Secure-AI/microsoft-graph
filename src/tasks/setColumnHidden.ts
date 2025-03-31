e371fc3ffcee10776b2732a75d240e3561928ef9
Ben Thompson <ben.thompson@futuresecure.ai>
2025-03-31 13:08:37 +1000

    Allowed return value

--- src/tasks/setColumnHidden.ts
+++ src/tasks/setColumnHidden.ts
@@ -1,9 +1,10 @@
+import type { WorkbookRange } from "@microsoft/microsoft-graph-types";
 import type { WorkbookRangeRef } from "../models/WorkbookRangeRef.ts";
 import updateWorkbookRange from "../operations/workbookRange/updateWorkbookRange.ts";
 
 /** Hide or show one or more columns. */
-export default async function setColumnHidden(rangeRef: WorkbookRangeRef, hidden: boolean): Promise<void> {
-	await updateWorkbookRange(rangeRef, {
+export default async function setColumnHidden(rangeRef: WorkbookRangeRef, hidden: boolean): Promise<WorkbookRange & WorkbookRangeRef> {
+	return await updateWorkbookRange(rangeRef, {
 		columnHidden: hidden,
 	});
 }
--- src/tasks/setRowHidden.ts
+++ src/tasks/setRowHidden.ts
@@ -1,9 +1,10 @@
+import type { WorkbookRange } from "@microsoft/microsoft-graph-types";
 import type { WorkbookRangeRef } from "../models/WorkbookRangeRef.ts";
 import updateWorkbookRange from "../operations/workbookRange/updateWorkbookRange.ts";
 
 /** Hide or show one or more rows. */
-export default async function setRowHidden(rangeRef: WorkbookRangeRef, hidden: boolean): Promise<void> {
-	await updateWorkbookRange(rangeRef, {
+export default async function setRowHidden(rangeRef: WorkbookRangeRef, hidden: boolean): Promise<WorkbookRange & WorkbookRangeRef> {
+	return await updateWorkbookRange(rangeRef, {
 		rowHidden: hidden,
 	});
 }
