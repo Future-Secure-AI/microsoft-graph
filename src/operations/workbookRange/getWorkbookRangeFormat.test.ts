cb68cfd90d3c916bdb03524901ed83b38546a542
Ben Thompson <ben.thompson@futuresecure.ai>
2025-03-31 13:06:27 +1000

    Added getWorkbookRangeFormat

---
+++ src/operations/workbookRange/getWorkbookRangeFormat.test.ts
@@ -0,0 +1,35 @@
+import { describe, expect, it } from "vitest";
+import { getDefaultDriveRef } from "../../services/drive.ts";
+import { driveItemPath } from "../../services/driveItem.ts";
+import { generateTempFileName } from "../../services/temporaryFiles.ts";
+import { createWorkbookRangeRef } from "../../services/workbookRange.ts";
+import { createWorkbookWorksheetRef, defaultWorkbookWorksheetId } from "../../services/workbookWorksheet.ts";
+import deleteDriveItemWithRetry from "../../tasks/deleteDriveItemWithRetry.ts";
+import createWorkbook from "../workbook/createWorkbook.ts";
+import getWorkbookRangeFormat from "./getWorkbookRangeFormat.ts";
+import updateWorkbookRange from "./updateWorkbookRange.ts";
+
+describe("getWorkbookRangeFormat", () => {
+	it("can retrieve the format of a range in an existing workbook", async () => {
+		const driveRef = getDefaultDriveRef();
+		const workbookPath = driveItemPath(generateTempFileName("xlsx"));
+		const workbook = await createWorkbook(driveRef, workbookPath);
+		const worksheetRef = createWorkbookWorksheetRef(workbook, defaultWorkbookWorksheetId);
+		const rangeRef = createWorkbookRangeRef(worksheetRef, "A1:B2");
+
+		try {
+			await updateWorkbookRange(rangeRef, {
+				values: [
+					[1, 2],
+					[3, 4],
+				],
+			});
+
+			const format = await getWorkbookRangeFormat(rangeRef);
+			expect(format.horizontalAlignment).toBe("General");
+			expect(format.verticalAlignment).toBe("Bottom");
+		} finally {
+			await deleteDriveItemWithRetry(workbook);
+		}
+	});
+});
---
+++ src/operations/workbookRange/getWorkbookRangeFormat.ts
@@ -0,0 +1,25 @@
+import type { WorkbookRangeFormat } from "@microsoft/microsoft-graph-types";
+import { operation } from "../../graphApi.ts";
+import type { GraphOperation } from "../../models/GraphOperation.ts";
+import type { WorkbookRangeRef } from "../../models/WorkbookRangeRef.ts";
+import { generatePath } from "../../services/templatedPaths.ts";
+
+/** Retrieve the format of a workbook range. @see https://learn.microsoft.com/en-us/graph/api/rangeformat-get */
+export default function getWorkbookRangeFormat(rangeRef: WorkbookRangeRef): GraphOperation<WorkbookRangeFormat & WorkbookRangeRef> {
+	return operation({
+		contextId: rangeRef.contextId,
+		method: "GET",
+		path: generatePath(`/sites/{site-id}/drives/{drive-id}/items/{item-id}/workbook/worksheets/{worksheet-id}/range(address='${rangeRef.address}')/format`, rangeRef),
+		headers: {
+			"workbook-session-id": rangeRef.sessionId,
+		},
+		body: null,
+		responseTransform: (response) => {
+			const format = response as WorkbookRangeFormat;
+			return {
+				...format,
+				...rangeRef,
+			};
+		},
+	});
+}
