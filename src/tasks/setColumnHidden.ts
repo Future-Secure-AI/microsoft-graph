5dd068e31f7ec60dcabc94b1675980c919bd5749
Ben Thompson <ben.thompson@futuresecure.ai>
2025-04-02 13:56:22 +1000

    Prevented updating all columns/rows

--- src/tasks/setColumnHidden.ts
+++ src/tasks/setColumnHidden.ts
@@ -2,7 +2,7 @@ import type { WorkbookRange } from "@microsoft/microsoft-graph-types";
 import type { RowRangeAddress } from "../models/Address.ts";
 import type { WorkbookRangeRef } from "../models/WorkbookRangeRef.ts";
 import updateWorkbookRange from "../operations/workbookRange/updateWorkbookRange.ts";
-import { decomposeAddress } from "../services/addressManipulation.ts";
+import { decomposeAddress, isAllColumnsAddress } from "../services/addressManipulation.ts";
 
 /**
  * Hide or show one or more columns.
@@ -12,6 +12,10 @@ import { decomposeAddress } from "../services/addressManipulation.ts";
  * @returns The updated workbook range.
  */
 export default async function setColumnHidden(rangeRef: WorkbookRangeRef, hidden: boolean): Promise<WorkbookRange & WorkbookRangeRef> {
+	if (isAllColumnsAddress(rangeRef.address)) {
+		throw new Error("Cannot hide all columns");
+	}
+
 	const components = decomposeAddress(rangeRef.address);
 	rangeRef.address = `${components.startColumn}:${components.endColumn}` as RowRangeAddress; // Workaround API explicitly wanting a range
 
--- src/tasks/setRowHidden.ts
+++ src/tasks/setRowHidden.ts
@@ -2,7 +2,7 @@ import type { WorkbookRange } from "@microsoft/microsoft-graph-types";
 import type { RowRangeAddress } from "../models/Address.ts";
 import type { WorkbookRangeRef } from "../models/WorkbookRangeRef.ts";
 import updateWorkbookRange from "../operations/workbookRange/updateWorkbookRange.ts";
-import { decomposeAddress } from "../services/addressManipulation.ts";
+import { decomposeAddress, isAllRowsAddress } from "../services/addressManipulation.ts";
 
 /**
  * Hide or show one or more rows.
@@ -12,6 +12,10 @@ import { decomposeAddress } from "../services/addressManipulation.ts";
  * @returns The updated workbook range.
  */
 export default async function setRowHidden(rangeRef: WorkbookRangeRef, hidden: boolean): Promise<WorkbookRange & WorkbookRangeRef> {
+	if (isAllRowsAddress(rangeRef.address)) {
+		throw new Error("Cannot hide all rows");
+	}
+
 	const components = decomposeAddress(rangeRef.address);
 	rangeRef.address = `${components.startRow}:${components.endRow}` as RowRangeAddress; // Workaround API explicitly wanting a range
 
