4b5af8512ce3dee913b50682e0d5a5b14dc1d767
Ben Thompson <ben.thompson@futuresecure.ai>
2025-04-03 10:55:29 +1000

    Bumped package

--- demo/log.ts
+++ demo/log.ts
@@ -1,19 +1,15 @@
 export function debug(msg: unknown): void {
-    // biome-ignore lint/suspicious/noConsole: <explanation>
-    console.debug(`\x1b[90m[DBG] ${msg}\x1b[0m`);
+	console.debug(`\x1b[90m[DBG] ${msg}\x1b[0m`);
 }
 
 export function info(msg: unknown): void {
-    // biome-ignore lint/suspicious/noConsole: <explanation>
-    console.info(`\x1b[37m[INF] ${msg}\x1b[0m`);
+	console.info(`\x1b[37m[INF] ${msg}\x1b[0m`);
 }
 
 export function warn(msg: unknown): void {
-    // biome-ignore lint/suspicious/noConsole: <explanation>
-    console.warn(`\x1b[33m[WRN] ${msg}\x1b[0m`);
+	console.warn(`\x1b[33m[WRN] ${msg}\x1b[0m`);
 }
 
 export function error(msg: unknown): void {
-    // biome-ignore lint/suspicious/noConsole: <explanation>
-    console.error(`\x1b[31m[ERR] ${msg}\x1b[0m`);
+	console.error(`\x1b[31m[ERR] ${msg}\x1b[0m`);
 }
