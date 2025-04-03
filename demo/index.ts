aa352ad77b14cb0a33a0e64fc76570a24c35866b
Ben Thompson <ben.thompson@futuresecure.ai>
2025-04-03 10:51:14 +1000

    Added auto limits and doco

--- demo/index.ts
+++ demo/index.ts
@@ -1,7 +1,6 @@
 import type { ClientId } from "../src/models/ClientId.ts";
 import type { ClientSecret } from "../src/models/ClientSecret.ts";
 import type { DriveId } from "../src/models/DriveId.ts";
-import type { HttpProxy } from "../src/models/HttpProxy.ts";
 import type { SiteId } from "../src/models/SiteId.ts";
 import type { TenantId } from "../src/models/TenantId.ts";
 import { register } from "../src/services/context.ts";
@@ -11,8 +10,8 @@ import { getEnvironmentVariable } from "../src/services/environmentVariable.ts";
 import { createSiteRef } from "../src/services/site.ts";
 import { generateTempFileName } from "../src/services/temporaryFiles.ts";
 import { createWorkbookRangeRef } from "../src/services/workbookRange.ts";
+import { createDefaultWorkbookWorksheetRef } from "../src/services/workbookWorksheet.ts";
 import createWorkbookAndStartSession from "../src/tasks/createWorkbookAndStartSession.ts";
-import getWorkbookWorksheetByName from "../src/tasks/getWorkbookWorksheetByName.ts";
 import iterateWorkbookRangeValues from "../src/tasks/iterateWorkbookRangeValues.ts";
 import safeDeleteWorkbook from "../src/tasks/safeDeleteWorkbook.ts";
 import setWorkbookRangeValues from "../src/tasks/setWorkbookRangeValues.ts";
@@ -24,27 +23,29 @@ const clientId = getEnvironmentVariable("AZURE_CLIENT_ID") as ClientId;
 const clientSecret = getEnvironmentVariable("AZURE_CLIENT_SECRET") as ClientSecret;
 const siteId = getEnvironmentVariable("SHAREPOINT_DEFAULT_SITE_ID") as SiteId;
 const driveId = getEnvironmentVariable("SHAREPOINT_DEFAULT_DRIVE_ID") as DriveId;
-const httpProxy = (getEnvironmentVariable("HTTP_PROXY", "") || undefined) as HttpProxy | undefined; // Not normally required
 
 info("Get drive...");
-const contextRef = register(tenantId, clientId, clientSecret, httpProxy);
+const contextRef = register(tenantId, clientId, clientSecret);
 const siteRef = createSiteRef(contextRef, siteId);
 const driveRef = createDriveRef(siteRef, driveId);
 
 info("Creating workbook...");
 const workbook = await createWorkbookAndStartSession(driveRef, generateTempFileName(workbookFileExtension));
 
-info("Setting range...");
-const worksheet = await getWorkbookWorksheetByName(workbook, "Sheet1");
-const rangeRef = createWorkbookRangeRef(worksheet, "A1:B3");
+info("Getting worksheet...");
+const worksheetRef = createDefaultWorkbookWorksheetRef(workbook); // OR `await getWorkbookWorksheetByName(workbook, "Sheet1");` to get one by name
+
+info("Writing range...");
+const rangeRef = createWorkbookRangeRef(worksheetRef, "A1:B3");
 await setWorkbookRangeValues(rangeRef, [
 	[1, 2],
 	[3, 4],
 	[5, 6],
 ]);
 
-info("Iterating range...");
+info("Reading range...");
 for await (const rowValues of iterateWorkbookRangeValues(rangeRef)) {
+	// This automatically uses multiple requests if the range is too big for a single request
 	debug(` ${rowValues}`);
 }
 
--- src/tasks/iterateWorkbookRangeValues.ts
+++ src/tasks/iterateWorkbookRangeValues.ts
@@ -1,16 +1,37 @@
+import InvalidArgumentError from "../errors/InvalidArgumentError.ts";
 import type { RowOffset } from "../models/RowOffset.ts";
 import type { RowRangeValues } from "../models/RowRangeValues.ts";
 import type { WorkbookRangeRef } from "../models/WorkbookRangeRef.ts";
 import getWorkbookWorksheetRange from "../operations/workbookRange/getWorkbookWorksheetRange.ts";
 import { composeAddress, decomposeAddress } from "../services/addressManipulation.ts";
-import { offsetToRowAddress, rowAddressToOffset } from "../services/addressOffset.ts";
+import { columnAddressToOffset, offsetToRowAddress, rowAddressToOffset } from "../services/addressOffset.ts";
 import { createWorkbookRangeRef } from "../services/workbookRange.ts";
 
-const defaultRowsPerRequest = 1000;
+/**
+ * Maximum number of cells that can be retrieved in a single request, unless overwritten.
+ * @remarks The Microsoft Graph API documentation does not specify a fixed maximum number of cells that can be retrieved in a single request.
+ * However, it mentions that large ranges may result in errors due to resource constraints. Additionally, discussions in developer
+ * communities suggest that requests exceeding 10,000 cells may encounter issues.
+ */
+const maxCellsPerRequest = 10_000;
 
-export default async function* iterateWorkbookRangeValues(rangeRef: WorkbookRangeRef, rowsPerRequest = defaultRowsPerRequest): AsyncIterable<RowRangeValues> {
+/**
+ * Iterates over the values of a workbook range in chunks, fetching data in manageable sizes.
+ *
+ * @param rangeRef - A reference to the workbook range to iterate over.
+ * @param overwriteRowsPerRequest - Optional. The number of rows to fetch per request. If omitted, it is automatically calculated.
+ * @returns An async iterable that yields rows of range values.
+ */
+export default async function* iterateWorkbookRangeValues(rangeRef: WorkbookRangeRef, overwriteRowsPerRequest: number | null = null): AsyncIterable<RowRangeValues> {
 	const address = rangeRef.address;
 	const components = decomposeAddress(address);
+	const columnsPerRow = columnAddressToOffset(components.endColumn) - columnAddressToOffset(components.startColumn) + 1;
+
+	if (overwriteRowsPerRequest !== null && overwriteRowsPerRequest < 1) {
+		throw new InvalidArgumentError("overwriteRowsPerRequest must be greater than 0");
+	}
+
+	const rowsPerRequest = overwriteRowsPerRequest ?? Math.floor(maxCellsPerRequest / columnsPerRow);
 
 	const rangeStartRowOffset = rowAddressToOffset(components.startRow);
 	const rangeEndRowOffset = rowAddressToOffset(components.endRow);
