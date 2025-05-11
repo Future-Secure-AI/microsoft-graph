aaf33f72be04fe3a07cf6d5b88e1884514f39839
Ben Thompson <ben.thompson@futuresecure.ai>
2025-05-12 08:14:12 +1000

    Fixed workbook name type

--- src/services/workbookWorksheet.ts
+++ src/services/workbookWorksheet.ts
@@ -1,12 +1,13 @@
 import type { WorkbookRef } from "../models/WorkbookRef.ts";
 import type { WorkbookWorksheetId } from "../models/WorkbookWorksheetId.ts";
+import type { WorkbookWorksheetName } from "../models/WorkbookWorksheetName.ts";
 import type { WorkbookWorksheetRef } from "../models/WorkbookWorksheetRef.ts";
 
 /** ID of the initial worksheet that is included in a new workbook. */
 export const defaultWorkbookWorksheetId = "{00000000-0001-0000-0000-000000000000}" as WorkbookWorksheetId; // Program Manager in Microsoft Office Extensibility team says this ID is used for the first sheet of all workbooks by design, but not documented. https://github.com/OfficeDev/office-js/issues/552#issuecomment-800841930
 
 /** Name of the initial worksheet that is included in a new workbook. */
-export const defaultWorkbookWorksheetName = "Sheet1";
+export const defaultWorkbookWorksheetName = "Sheet1" as WorkbookWorksheetName;
 
 /**
  * Creates a reference to a workbook worksheet.
