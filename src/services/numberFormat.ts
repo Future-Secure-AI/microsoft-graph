3ce8c3f639a2adde455c3f609f6d9ecb80d5741a
Ben Thompson <ben.thompson@futuresecure.ai>
2025-05-29 11:27:26 +1000

    Added default number formats

--- src/services/numberFormat.ts
+++ src/services/numberFormat.ts
@@ -1,3 +1,10 @@
 import type { NumberFormat } from "../models/NumberFormat.ts";
 
 export const generalNumberFormat = "General" as NumberFormat;
+export const numberNumberFormat = "#,##0.00" as NumberFormat;
+export const currencyNumberFormat = "$#,##0.00" as NumberFormat;
+export const accountingNumberFormat = '_($* #,##0.00_);_($* (#,##0.00);_($* "-"??_);_(@_)' as NumberFormat;
+export const percentageNumberFormat = "0.00%" as NumberFormat;
+export const dateShortNumberFormat = "dd/mm/yyyy" as NumberFormat;
+export const timeNumberFormat = "h:mm:ss AM/PM" as NumberFormat;
+export const textNumberFormat = "@" as NumberFormat;
