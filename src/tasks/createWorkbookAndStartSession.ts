d9eaf6f96d2cf2d938e376b863dfa74ff4cb9b1a
Ben Thompson <ben.thompson@futuresecure.ai>
2025-05-29 10:15:04 +1000

    Fixed possibility of creating a workbook in a folder

--- src/tasks/createWorkbookAndStartSession.ts
+++ src/tasks/createWorkbookAndStartSession.ts
@@ -1,5 +1,6 @@
 import type { Workbook } from "@microsoft/microsoft-graph-types";
 import type { DriveItemPath } from "../models/DriveItemPath.ts";
+import type { DriveItemRef } from "../models/DriveItemRef.ts";
 import type { DriveRef } from "../models/DriveRef.ts";
 import type { WorkbookRef } from "../models/WorkbookRef.ts";
 import createWorkbook from "../operations/workbook/createWorkbook.ts";
@@ -8,12 +9,12 @@ import createWorkbookSession from "../operations/workbookSession/createWorkbookS
 /**
  * Create a new workbook and open a session for that workbook in a single operation.
  *
- * @param driveRef - A reference to the drive where the workbook will be created.
+ * @param parentRef - A reference to the drive or driveItem where the workbook will be created.
  * @param itemPath - The path of the new workbook within the drive.
  * @returns The created workbook and session details.
  */
-export default async function createWorkbookAndStartSession(driveRef: DriveRef, itemPath: DriveItemPath): Promise<Workbook & WorkbookRef> {
-	const workbook = await createWorkbook(driveRef, itemPath);
+export default async function createWorkbookAndStartSession(parentRef: DriveRef | DriveItemRef, itemPath: DriveItemPath): Promise<Workbook & WorkbookRef> {
+	const workbook = await createWorkbook(parentRef, itemPath);
 	const workbookRef = await createWorkbookSession(workbook);
 
 	return {
