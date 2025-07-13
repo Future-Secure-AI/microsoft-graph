2f0bdec2dfba11eead792bdad3a52dae057b196f
Ben Thompson <ben.thompson@futuresecure.ai>
2025-07-14 07:54:18 +1000

    Fixed typing and remove .npmrc

--- src/tasks/tryGetDriveItemByPath.ts
+++ src/tasks/tryGetDriveItemByPath.ts
@@ -10,16 +10,16 @@ import getDriveItemByPath from "../operations/driveItem/getDriveItemByPath.ts";
 import { isErrorWithName } from "../services/error.ts";
 
 /**
- * Attempts to retrieve the metadata for an item in a drive by file path.
+ * Attempts to retrieve the metadata for an item in a drive or folder by file path.
  * Returns null if the item does not exist (NotFoundError).
- * @param driveRef Reference to the drive containing the item.
+ * @param parentRef Reference to the drive or folder containing the item.
  * @param itemPath Path of the item within the drive.
  * @returns The metadata of the specified drive item, or null if not found.
  * @see https://learn.microsoft.com/en-us/graph/api/driveitem-get
  */
-export default async function tryGetDriveItemByPath(driveRef: DriveRef, itemPath: DriveItemPath): Promise<(DriveItem & DriveItemRef) | null> {
+export default async function tryGetDriveItemByPath(parentRef: DriveRef | DriveItemRef, itemPath: DriveItemPath): Promise<(DriveItem & DriveItemRef) | null> {
 	try {
-		return await getDriveItemByPath(driveRef, itemPath);
+		return await getDriveItemByPath(parentRef, itemPath);
 	} catch (error) {
 		if (isErrorWithName(error, "NotFoundError")) {
 			return null;
